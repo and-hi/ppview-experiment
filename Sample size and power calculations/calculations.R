d510b659a070c9e21ab4122ec03b70ba2565234f
and-hi <and-hi@posteo.eu>
2020-08-24 11:43:04 +0200

    More futile prior predictive tries

--- Sample size and power calculations/calculations.R
+++ Sample size and power calculations/calculations.R
@@ -26,8 +26,6 @@ pre_meat_servings <- runif(n, min_meat, max_meat)
 # Simulate model parameters
 ###
 
-beta_0 <- 10
-
 beta_gender <- rep.int(rnorm(1, mean = 0, sd =1), 100)
 beta_treatment <- rep.int(rnorm(1, mean = -5, sd =5), 100)
 beta_treatment_gender <- rep.int(rnorm(1, mean = 0, sd =1), 100)
@@ -62,7 +60,7 @@ meat_long = tibble(id = rep(seq(1:100),2), time = c(rep("pre", 100), rep("post",
 ggplot(data = meat_long, aes(x = time, y = meat_servings, group = id)) + geom_line()
 
 ###
-# Prior predictive check
+# Prior predictive check with fixed pre_meat_servings
 ###
 
 N = 44
@@ -75,9 +73,46 @@ prior_predictive_stanFit = sampling(object=prior_predictive_DSO , data=prior_pre
                            chains=4 , iter=1000 , warmup=0 , thin=1,
                            algorithm="Fixed_param")
 plot(prior_predictive_stanFit, pars = c("beta_treatment", "post_meat_servings_sim[1]", "post_meat_servings_sim[23]", "post_meat_servings_sim[2]", "post_meat_servings_sim[24]"), show_density=TRUE)
+plot(prior_predictive_stanFit, pars = c("beta_treatment", "post_meat_servings_sim[21]", "post_meat_servings_sim[43]", "post_meat_servings_sim[22]", "post_meat_servings_sim[44]"), show_density=TRUE)
 print(prior_predictive_stanFit)
 
+df <- as.data.frame(prior_predictive_stanFit)
+summary(prior_predictive_stanFit@sim['samples'][[1]][[1]]$'post_meat_servings_sim[43]'/prior_predictive_stanFit@sim['samples'][[1]][[1]]$'post_meat_servings_sim[21]')
+summary(prior_predictive_stanFit@sim['samples'][[1]][[1]]$'post_meat_servings_sim[2]'/prior_predictive_stanFit@sim['samples'][[1]][[1]]$'post_meat_servings_sim[24]')
 
+str(prior_predictive_stanFit@sim['samples'][[1]][[1]])
+
+###
+# Prior predictive check with random pre_meat_servings
+###
+
+n = 100
+N = 2
+pre_meat_servings_random = rep(10,2)  #rpois(N, 10)
+treatment = c(rep(0,N/2), rep(1,N/2))
+prior_predictive_data_random = list(N = N, pre_meat_servings = pre_meat_servings_random, treatment=treatment)
+prior_predictive_DSO = stan_model(file = 'Sample size and power calculations/prior_predictive.stan')
+prior_predictive_stanFit_random = sampling(object=prior_predictive_DSO, data=prior_predictive_data_random ,
+                                    chains=1 , iter=n , warmup=0 , thin=1,
+                                    algorithm="Fixed_param")
+
+print(prior_predictive_stanFit_random)
+plot(prior_predictive_stanFit_random)
+
+data_ppc_random <- as.data.frame(prior_predictive_stanFit_random)
+
+data_ppc_random %>%
+  select(`post_meat_servings_sim[1]`, `post_meat_servings_sim[2]`) %>%
+  stack() %>%
+  mutate(treatment = ifelse(ind=="post_meat_servings_sim[1]",0,1)) %>%
+  rename(post_meat_servings = values) %>%
+  select(-ind) -> data_simu
+
+
+
+
+
+####
 invgamma = stan(file = 'Sample size and power calculations/inv_gamma.stan', algorithm="Fixed_param")
 plot(invgamma)
 exp(20.7944)
--- Sample size and power calculations/prior_predictive.stan
+++ Sample size and power calculations/prior_predictive.stan
@@ -13,11 +13,11 @@ data {
 //   real beta_treatment = normal_rng(-0.2, 0.1);
 //   int<lower = 0> post_meat_servings_sim[N] = poisson_rng(pre_meat_servings .* exp(beta_treatment * treatment) + 0.01);
 // }
-// generated quantities {
-//   real beta_treatment = normal_rng(-0.2, 0.1);
-//   int<lower = 0> post_meat_servings_sim[N] = poisson_log_rng(log(pre_meat_servings + 0.01) + beta_treatment * treatment);
-// }
 generated quantities {
-  real beta_treatment = lognormal_rng(-0.2, 0.1);
-  int<lower = 0> post_meat_servings_sim[N] = poisson_log_rng(log(pre_meat_servings .* (beta_treatment * treatment) +0.01));
+  real beta_treatment = normal_rng(-0.2, 0.1);
+  int<lower = 0> post_meat_servings_sim[N] = poisson_log_rng(log(pre_meat_servings + 0.01) + beta_treatment * treatment);
 }
+// generated quantities {
+//   real beta_treatment = lognormal_rng(-0.2, 0.1);
+//   int<lower = 0> post_meat_servings_sim[N] = poisson_log_rng(log(pre_meat_servings+0.01) .* (beta_treatment * treatment) );
+// }
