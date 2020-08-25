e5bb4d91e469e50d7b5a0b612cabb3b467acaf0a
and-hi <and-hi@posteo.eu>
2020-08-25 12:54:30 +0200

    Adds visualizations for simulated data

--- Sample size and power calculations/sample_size_calculations.R
+++ Sample size and power calculations/sample_size_calculations.R
@@ -1,63 +1,17 @@
 library(ggplot2)
 library(dplyr)
-
-###
-# Simulate independent variables
-###
-
-n <- 100
-
-treatment <- c(rep.int(c(rep.int(0, n/4), rep.int(1, n/4)),2))
-
-min_meat <- 0
-max_meat <- 21
-pre_meat_servings <- runif(n, min_meat, max_meat)
-
-###
-# Simulate model parameters
-###
-
-
-beta_treatment <- rep.int(rnorm(1, mean = -5, sd =5), 100)
-
-
-###
-# Simulate dependent variables
-###
-
-post_meat_servings <- pre_meat_servings + beta_treatment*treatment
-# missing noise
-
-###
-# Fit model
-###
-
-ols <- glm( post_meat_servings ~ pre_meat_servings + treatment*gender)
-
-summary(ols)
-
-###
-# Visualizations
-###
-data_simu = tibble(id = seq(1:100), post_meat_servings, pre_meat_servings, gender, treatment = as.factor(treatment), age, beta_gender, beta_treatment, beta_treatment_gender)
-qplot(x = treatment, y = post_meat_servings)
-
-ggplot(data = data_simu, aes(x = treatment, y = post_meat_servings, group= treatment)) + geom_boxplot()
-
-
-
-meat_long = tibble(id = rep(seq(1:100),2), time = c(rep("pre", 100), rep("post", 100)), meat_servings = c(pre_meat_servings, post_meat_servings))
-ggplot(data = meat_long, aes(x = time, y = meat_servings, group = id)) + geom_line()
-
-
-####
+library(tidyr)
 
 simulate_data <- function(n, treatment_effect){
+  # Simulate independent variables
   min_meat <- 0
   max_meat <- 21
   pre_meat_servings <- round(runif(n, min_meat, max_meat), digits = 0)
   treatment <- c(rep(0, n/2), rep(1, n/2))
+  
+  # Simulate dependent variables
   post_meat_servings = rpois(n, pre_meat_servings * exp(treatment_effect * treatment) + 0.01)
+  
   simulated_data = tibble(post_meat_servings, pre_meat_servings, treatment)
   return(simulated_data)
 }
@@ -65,21 +19,59 @@ simulate_data <- function(n, treatment_effect){
 sim_data_and_fit_model <- function(sample_size, number_simulations) {
   treatment_effects = rnorm(number_simulations, -0.2, 0.1)
   data = vector("list", number_simulations)
-  glm_fit = vector("list", number_simulations)
+  glm_fits = vector("list", number_simulations)
   for (i in seq_len(number_simulations)) {
     data[[i]] <- simulate_data(sample_size, treatment_effects[i])
-    glm_fit[[i]] <- glm( post_meat_servings ~ pre_meat_servings + treatment,
+    glm_fits[[i]] <- glm( post_meat_servings ~ pre_meat_servings + treatment,
                 data = data[[i]],
                 family = "poisson"
                 )
   }
-  return(data, glm_fit)
-  
+  return(list(data = data, glm_fits = glm_fits))
 }
 
-sim_data = simulate_data(10, -0.2)
+sim_data = simulate_data(100, -0.2)
 
-list_of_sims = sim_data_and_fit_model(10, 5)
-sample_size =10
-number_simulations = 5
-i =1
+sample_size = 160
+number_simulations = 1000
+list_of_sims = sim_data_and_fit_model(sample_size, number_simulations)
+
+coefficients(summary(list_of_sims[["glm_fits"]][[1]]))[3,4]
+
+###
+# Descriptive Stats
+###
+
+significants = rep(0, number_simulations)
+for(i in seq_len(number_simulations)){
+  significants[[i]] = coefficients(summary(list_of_sims[["glm_fits"]][[i]]))[3,4]
+}
+
+summary(significants)
+
+mean(significants < 0.05)
+
+###
+# Visualizations
+###
+
+ggplot(data = sim_data, aes(x = treatment, y = post_meat_servings, group= treatment)) +
+  geom_violin(draw_quantiles = c(0.05,0.25,0.5,0.75,0.95))
+sim_data <- sim_data %>%
+  mutate(diff_meat_servings=post_meat_servings-pre_meat_servings)
+ggplot(data = sim_data, aes(x = treatment, y = diff_meat_servings, group= treatment)) +
+  geom_violin(draw_quantiles = c(0.05,0.25,0.5,0.75,0.95))
+
+meat_long = sim_data %>%
+  mutate(id = 1:nrow(sim_data)) %>%
+  pivot_longer(c(post_meat_servings, pre_meat_servings),names_to = "time", values_to = "meat_servings") %>%
+  mutate(time=factor(time, levels = c("pre_meat_servings", "post_meat_servings")))
+ggplot(data = meat_long, aes(x = time, y = meat_servings, group = id, color = factor(treatment))) + geom_line()
+
+
+####
+
+
+
+
+
